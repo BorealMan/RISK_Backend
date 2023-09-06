e9bf5817f34651543a01a10c11ec165b90febd08
RK <rkehrli1993@gmail.com>
2023-09-06 15:24:44 -0400

    updated readme instructions. added comments and game dict.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Risk Backend
+
+A backend for the RISK board game.
+
+## Requirements
+
+- Node JS
+- npm
+
+## Installation Instructions
+
+- Pull the repository
+- npm install
+- node index.js
--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import CONFIG from './config.js'
 import express from './node_modules/express/index.js'
 import cors from './node_modules/cors/lib/index.js'
 
+// Init Express API
 const app = express()
 const icors = cors()
-
 app.use(icors)
+
+// Game Dict
+const Games = {};
+
+app.get('/newgame', (req, res) => {
+    // Create a new game and return the key
+})
 
 app.get('/', (req, res) => {
     const msg = `<h1>Hello World</h1>`;
