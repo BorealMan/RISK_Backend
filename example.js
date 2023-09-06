759bd802162ebfd4e446cfd7d87c3da0b726f7c9
RK <rkehrli1993@gmail.com>
2023-09-05 20:15:41 -0400

    added experimental game code and readme

---
+++ example.js
@@ -0,0 +1,41 @@
+/* 
+    Big Picture View Of Game Objects - Data Structures
+*/
+
+const game = {
+    continents: [{}],
+    territories: [{}],
+    players: [{}],
+    cards: [{}],
+    messages: [{}],
+    currentTurn: 1, // playerID
+    uuid: "",
+}
+
+const continent = {
+    id: 1,
+    name: "North America",
+    player: undefined,
+}
+
+const territory = {
+    id: 1,
+    continent: 1,
+    player: undefined, // playerID
+    name: "Alaska",
+    troops: 0,
+    connections: [] // territoryID
+}
+
+const player = {
+    id: 1,
+    username: "",
+    troops: 0,
+    deployable_troops: 0,
+}
+
+const riskCard = {
+    id: 1,
+    name: "",
+    description: "",
+}
