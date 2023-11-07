e2f397a904592cbedb1ac6ddb89315a06e6faff6
RK <rkehrli1993@gmail.com>
2023-11-06 23:54:21 -0500

    added time since comparison function.

--- utils/time.js
+++ utils/time.js
@@ -2,17 +2,23 @@ export function Sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Returns Current Unix Time In Milli-Seconds
 export function GetUnixTime() {
     const d = new Date()
     const t = d.getTime()
     return t
 }
 
-// Return In MilliSeconds - Assumes First Time Is First
+// Returns Difference Between Two Times In Seconds - Assumes Earliest Time Is T1
 export function UnixTimeDiff(t1, t2, seconds=true) {
     let diff = t2 - t1
     if (seconds) {
         diff /= 1000        
     }
     return diff
+}
+
+// Returns Difference Between Past Time And Now
+export function UnixTimeSince(time) {
+    return UnixTimeDiff(time, GetUnixTime())
 }
